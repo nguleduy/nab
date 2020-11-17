ca0896115a11386ac82aee2eebfd1ba103ab89e6
Joseph <nguld@itsj-group.com>
2020-11-17 14:30:07 +0700

    added cart-service

---
+++ cart-service/src/test/java/com/example/joseph/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joseph.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
